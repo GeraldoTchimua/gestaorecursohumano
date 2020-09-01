53c100753a2aa0a1ba01f735a54ec5ddbd59db4d
GENATCHI <GENATCHI@DESKTOP-H9R4G1G>
2020-09-01 12:53:32 +0100

    HR com Bootstrap1

--- src/main/java/rh/controle/FuncionarioMBean.java
+++ src/main/java/rh/controle/FuncionarioMBean.java
@@ -1,6 +1,7 @@
-
 package rh.controle;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
@@ -13,32 +14,42 @@ import rh.modelo.Funcionario;
 @Named(value = "funcionarioMBean")
 @RequestScoped
 public class FuncionarioMBean {
-    Funcionario funcionario = new Funcionario();
-  
-  
+
+    Funcionario funcionario1 = new Funcionario("Geraldo","Genatchi","Informatica",45,5000.00);
+    Funcionario funcionario2 = new Funcionario("Samuel","PCS","Direito",30,200000.00);
+    Funcionario funcionario3 = new Funcionario("Gaudencio","Wassuca","Filosofia",25,200000.00);
+    
+    List<Funcionario> funcionarios = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        funcionarios.add(funcionario1);
+        funcionarios.add(funcionario2);
+        funcionarios.add(funcionario3);
+    }
+    
     
 
-   @PostConstruct
-   public void init(){
-   }
-
-    public Funcionario getFuncionario() {
+   /* public Funcionario getFuncionario() {
         return funcionario;
     }
 
     public void setFuncionario(Funcionario funcionario) {
         this.funcionario = funcionario;
-    }
-    
-   
-    
+    }*/
+
     //depois de clicar no botao guardar, o clique nos leva a uma pagina de nome resultado
-    public String guardar(){
-        
-        
+    /*public String guardar() {
+
         return "resultado";
+    }*/
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
     }
-   
-   
-    
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
 }
--- src/main/java/rh/modelo/Funcionario.java
+++ src/main/java/rh/modelo/Funcionario.java
@@ -8,11 +8,47 @@ package rh.modelo;
 public class Funcionario {
     private String nome;
     private String sobrenome;
-    private double peso;
+    private String departamento;
+    private int idade;
+    private double salario;
+    
+    /*private double peso;
     private double altura;
-    private double imc;
+    private double imc;*/
 
     public Funcionario() {
+    }
+
+    public Funcionario(String nome, String sobrenome, String departamento, int idade, double salario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.departamento = departamento;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
     public String getNome() {
@@ -31,29 +67,8 @@ public class Funcionario {
         this.sobrenome = sobrenome;
     }
 
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
-
-    public double getImc() {
-        return imc;
-    }
-
-    public void setImc(double imc) {
-        this.imc = imc;
-    }
+    
+    
     
     
     
